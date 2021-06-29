66f9e0170dfc39dd49e7a8b6b8c22965c0eb4de5
long.woo <longwunet@outlook.com>
2021-06-29 22:43:50 +0800

    feat: init commit

---
+++ rollup.config.js
@@ -0,0 +1,106 @@
+import path from 'path';
+import resolve from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
+import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
+import progress from 'rollup-plugin-progress';
+
+import pkg from './package.json';
+
+/**
+ * 文件的绝对路径
+ * @param {*} p 目录名
+ */
+const pathResolve = p => path.resolve(__dirname, p);
+
+/**
+ * 文件头说明
+ * @param {*} name 包名
+ * @param {*} fileName 文件名
+ * @param {*} version 版本号
+ */
+const generateBanner = (name, fileName, version) => {
+  const currentYear = new Date().getFullYear();
+  const year = currentYear === 2021 ? currentYear : `2020-${currentYear}`;
+
+  return `/*! **************************************************
+** ${name}(${fileName}) version ${version}
+** (c) ${year} long.woo
+** https://github.com/long-woo/sentry-sourcemap-plugin
+*************************************************** */\n`;
+};
+
+const buildFormat = fileName => ({
+  cjs: {
+    outFile: `${fileName}.js`,
+    format: 'cjs',
+    mode: 'development'
+  },
+  'cjs-prod': {
+    outFile: `${fileName}.min.js`,
+    format: 'cjs',
+    mode: 'production'
+  },
+  es: {
+    outFile: `${fileName}.es.js`,
+    format: 'es',
+    mode: 'development'
+  },
+  'es-prod': {
+    outFile: `${fileName}.es.min.js`,
+    format: 'es',
+    mode: 'production'
+  }
+});
+
+/**
+ * 获取配置
+ * @param {*} param
+ * outFile 输出文件名
+ * format 编译文件类型
+ * mode 编译环境
+ */
+const getConfig = ({ outFile, format, mode }) => {
+  const isProduction = mode === 'production';
+
+  const version = pkg.version;
+  const external = Object.keys({ ...pkg.dependencies || ''});
+
+  const globals = external.reduce((prev, current) => {
+    prev[current] = current;
+
+    return prev;
+  }, {});
+
+  return {
+    input: pathResolve(`src/index.ts`),
+    output: {
+      file: pathResolve(`dist/${outFile}`),
+      banner: generateBanner(pkg.name, outFile, version),
+      globals,
+      format,
+      exports: format !== 'es' ? 'named' : 'auto'
+    },
+    plugins: [
+      typescript({
+        exclude: 'node_modules/**',
+        typescript: require('typescript'),
+        useTsconfigDeclarationDir: true
+      }),
+      resolve(),
+      json(),
+      isProduction && terser(),
+      progress()
+    ],
+    external
+  };
+};
+
+const build = () => {
+  const format = buildFormat(pkg.displayName);
+  return Object.keys(format).map(key => getConfig(format[key]));
+};
+
+const buildConfig = build();
+
+export default buildConfig;
---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import { Compiler } from 'webpack';
+import SentryWebpackPlugin, { SentryCliPluginOptions } from '@sentry/webpack-plugin';
+import rimraf from 'rimraf'
+
+interface ISentrySourcemapPluginOptions {
+  /**
+   * Map 文件地址。默认 `dist` 目录下的所有 .map 文件
+   */
+  path?: string;
+
+  /**
+   * Sentry 配置
+   */
+  sentryConfig: SentryCliPluginOptions
+}
+
+class SentrySourcemapPlugin {
+  private readonly pluginName = 'SentrySourcemapPlugin'
+  private readonly path:string = './dist/**/*.map'
+  private readonly sentryConfig: SentryCliPluginOptions = {
+    release: '1.0.0',
+    include: './dist',
+    configFile: '.sentryclirc'
+  }
+
+  constructor(options: ISentrySourcemapPluginOptions) {
+    this.path = options.path
+    this.sentryConfig = options.sentryConfig
+  }
+
+  /**
+   * 注入 Sentry 的 Webpack 插件
+   */
+  injectSentryPlugin() {
+    new SentryWebpackPlugin(this.sentryConfig)
+  }
+
+  apply(compiler: Compiler) {
+    this.injectSentryPlugin()
+
+    // 编译完成后，移除 map 文件
+    compiler.hooks.done.tap(this.pluginName, () => {
+      rimraf.sync(this.path)
+    })
+  }
+}
+
+export { SentrySourcemapPlugin }
