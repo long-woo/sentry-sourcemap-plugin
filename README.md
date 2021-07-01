b1e73502934b7d5f057ecccbd8d8ce270534a472
long.woo <longwunet@outlook.com>
2021-07-01 15:05:10 +0800

    chore: v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # sentry-sourcemap-plugin
-Sentry Webpack 插件
+
+> 扩展 `@sentry/webpack-plugin` 插件。
+
+Sentry 上传 sourcemap 文件后，自动删除 .map 文件。
+
+⚠️ 此插件默认配置了一些初始值：
+- release: '1.0.0'，自动读取当前项目 `package.json` 中的 `version`。
+- include: './dist'。
+- configFile: '.sentryclirc'。
+
+## 使用
+
+### 安装
+
+> @longwoo/sentry-sourcemap-plugin 已经包含 @sentry/webpack-plugin 插件，无需安装它。
+
+```sh
+npm install @longwoo/sentry-sourcemap-plugin --save-dev
+# 或
+yarn add @longwoo/sentry-sourcemap-plugin -D
+```
+
+### vue 项目中，在 `vue.config.js` 文件添加如下配置：
+
+```js
+const { SentrySourcemapPlugin } = require('@longwoo/sentry-sourcemap-plugin')
+
+module.exports = {
+  configureWebpack: config => {
+    config.plugins.push(new SentrySourcemapPlugin())
+  }
+}
+```
+
+### 其他项目，在 `webpack.config.js` 文件的 `plugins` 选项中增加。
+
+```js
+const { SentrySourcemapPlugin } = require('@longwoo/sentry-sourcemap-plugin')
+
+module.exports = {
+  plugins: [
+    new SentrySourcemapPlugin()
+  ]
+}
+```
--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,61 @@
+import fs from 'fs'
 import { Compiler } from 'webpack';
-import { SentryCliPluginOptions } from '@sentry/webpack-plugin';
+import SentryWebpackPlugin, { SentryCliPluginOptions } from '@sentry/webpack-plugin';
 import rimraf from 'rimraf'
-const SentryWebpackPlugin = require('@sentry/webpack-plugin')
 
-interface ISentrySourcemapPluginOptions {
-  /**
-   * Map 文件地址。默认 `dist` 目录下的所有 .map 文件
-   */
-  path?: string;
-
-  /**
-   * Sentry 配置
-   */
-  sentryConfig: SentryCliPluginOptions
-}
+// 默认包含路径
+const DEFAULT_INCLUDE = './dist'
 
 class SentrySourcemapPlugin {
   private readonly pluginName = 'SentrySourcemapPlugin'
-  private readonly path:string = './dist/**/*.map'
+  private readonly mapFilePath:string = `${DEFAULT_INCLUDE}/**/*.map`
   private readonly sentryConfig: SentryCliPluginOptions = {
     release: '1.0.0',
-    include: './dist',
+    include: DEFAULT_INCLUDE,
     configFile: '.sentryclirc'
   }
 
-  constructor(options: ISentrySourcemapPluginOptions) {
-    if (options?.path) {
-      this.path = options.path
+  constructor(options: SentryCliPluginOptions) {
+    const release = this.getRelease()
+
+    this.sentryConfig = {
+      ...this.sentryConfig,
+      release,
+      ...options
     }
-    this.sentryConfig = options?.sentryConfig
+    this.mapFilePath = `${this.sentryConfig.include}/**/*.map`
+  }
+
+  /**
+   * 获取 package.json 文件的 version
+   */
+  getRelease() {
+    const fileContent = fs.readFileSync('./package.json', { encoding: 'utf-8' })
+    const json = JSON.parse(fileContent)
+    
+    return json.version
   }
 
   /**
    * 注入 Sentry 的 Webpack 插件
    */
-  injectSentryPlugin() {
-    new SentryWebpackPlugin(this.sentryConfig)
+  injectSentryPlugin(compiler: Compiler) {
+    const sentryWebpackPlugin = new SentryWebpackPlugin(this.sentryConfig)
+
+    compiler.options.plugins.push(sentryWebpackPlugin)
   }
 
+  /**
+   * 插件 apply 方法
+   * @param compiler - Webpack 插件 compiler 对象
+   */
   apply(compiler: Compiler) {
-    this.injectSentryPlugin()
-
+    this.injectSentryPlugin(compiler)
+    
     // 编译完成后，移除 map 文件
     compiler.hooks.done.tap(this.pluginName, () => {
-      rimraf.sync(this.path)
+      rimraf.sync(this.mapFilePath)
+      console.log('.map 文件删除完成')
     })
   }
 }
